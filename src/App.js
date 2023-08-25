013e68a84ca6b4ddb95761abec6b877dce1ba7be
Rehan Ankalgi <rehanankalgi7t2@gmail.com>
2023-08-25 15:55:50 +0530

    project setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import logo from "./logo.svg";
+
+function App() {
+  return (
+    <div className="App">
+      <img src={logo} alt="cadabams-logo" className="App-logo" />
+      <h1>Cadabams Group</h1>
+    </div>
+  );
+}
+
+export default App;
